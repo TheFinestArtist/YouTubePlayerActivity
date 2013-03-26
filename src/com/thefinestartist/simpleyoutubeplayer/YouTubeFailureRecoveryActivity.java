c29919877e4da928f18ba2653b32bf36320e79c2
TheFinestArtist <liger745547@gmail.com>
2013-03-26 19:06:51 +0900

    First Commit

---
+++ src/com/thefinestartist/simpleyoutubeplayer/YouTubeFailureRecoveryActivity.java
@@ -0,0 +1,38 @@
+
+package com.thefinestartist.simpleyoutubeplayer;
+
+import android.content.Intent;
+import android.widget.Toast;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+
+public abstract class YouTubeFailureRecoveryActivity extends YouTubeBaseActivity implements
+        YouTubePlayer.OnInitializedListener {
+
+    private static final int RECOVERY_DIALOG_REQUEST = 1;
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider,
+            YouTubeInitializationResult errorReason) {
+        if (errorReason.isUserRecoverableError()) {
+            errorReason.getErrorDialog(this, RECOVERY_DIALOG_REQUEST).show();
+        } else {
+            String errorMessage = String.format(getString(R.string.error_player),
+                    errorReason.toString());
+            Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == RECOVERY_DIALOG_REQUEST) {
+            // Retry initialization if user performed a recovery action
+            getYouTubePlayerProvider().initialize(getString(R.string.google_api_key), this);
+        }
+    }
+
+    protected abstract YouTubePlayer.Provider getYouTubePlayerProvider();
+
+}
---
+++ src/com/thefinestartist/simpleyoutubeplayer/YouTubePlayerActivity.java
@@ -0,0 +1,177 @@
+
+package com.thefinestartist.simpleyoutubeplayer;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.media.AudioManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.KeyEvent;
+import android.widget.Toast;
+
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayer.OnFullscreenListener;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class YouTubePlayerActivity extends YouTubeFailureRecoveryActivity implements
+        OnFullscreenListener {
+
+    public static final String EXTRA_VIDEO_ID = "video_id";
+    private static final boolean TOAST = false;
+
+    @SuppressLint("InlinedApi")
+    private static final int PORTRAIT_ORIENTATION = Build.VERSION.SDK_INT < 9
+            ? ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+            : ActivityInfo.SCREEN_ORIENTATION_SENSOR_PORTRAIT;
+
+    @SuppressLint("InlinedApi")
+    private static final int LANDSCAPE_ORIENTATION = Build.VERSION.SDK_INT < 9
+            ? ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+            : ActivityInfo.SCREEN_ORIENTATION_SENSOR_LANDSCAPE;
+
+    private YouTubePlayerView mPlayerView;
+    private String mVideoId = null;
+    private YouTubePlayer mPlayer = null;
+    private boolean mAutoRotation = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.act_youtube_player);
+        mPlayerView = (YouTubePlayerView) findViewById(R.id.player);
+        mPlayerView.initialize(getString(R.string.google_api_key), this);
+        mVideoId = getIntent().getStringExtra(EXTRA_VIDEO_ID);
+
+        mAutoRotation = Settings.System.getInt(getContentResolver(),
+                Settings.System.ACCELEROMETER_ROTATION, 0) == 1;
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer player,
+            boolean wasRestored) {
+        mPlayer = player;
+        if (mAutoRotation) {
+            player.addFullscreenControlFlag(YouTubePlayer.FULLSCREEN_FLAG_CONTROL_ORIENTATION
+                    | YouTubePlayer.FULLSCREEN_FLAG_CONTROL_SYSTEM_UI
+                    | YouTubePlayer.FULLSCREEN_FLAG_ALWAYS_FULLSCREEN_IN_LANDSCAPE
+                    | YouTubePlayer.FULLSCREEN_FLAG_CUSTOM_LAYOUT);
+        } else {
+            player.addFullscreenControlFlag(YouTubePlayer.FULLSCREEN_FLAG_CONTROL_ORIENTATION
+                    | YouTubePlayer.FULLSCREEN_FLAG_CONTROL_SYSTEM_UI
+                    | YouTubePlayer.FULLSCREEN_FLAG_CUSTOM_LAYOUT);
+        }
+        if (!wasRestored) {
+            player.loadVideo(mVideoId);
+            player.setOnFullscreenListener(this);
+        }
+    }
+
+    @Override
+    protected YouTubePlayer.Provider getYouTubePlayerProvider() {
+        return mPlayerView;
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            if (mPlayer != null)
+                mPlayer.setFullscreen(true);
+            if (TOAST)
+                Toast.makeText(this, "landscape", Toast.LENGTH_SHORT).show();
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            if (mPlayer != null)
+                mPlayer.setFullscreen(false);
+            if (TOAST)
+                Toast.makeText(this, "portrait", Toast.LENGTH_SHORT).show();
+        } else {
+            if (TOAST)
+                Toast.makeText(this, "configuration changed", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onFullscreen(boolean fullsize) {
+        if (TOAST)
+            Toast.makeText(this, "full size change : " + fullsize, Toast.LENGTH_SHORT).show();
+        if (fullsize) {
+            setRequestedOrientation(LANDSCAPE_ORIENTATION);
+        } else {
+            setRequestedOrientation(PORTRAIT_ORIENTATION);
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if (keyCode == KeyEvent.KEYCODE_VOLUME_UP) {
+            AudioManager audioManager = (AudioManager) getBaseContext().getSystemService(
+                    Context.AUDIO_SERVICE);
+            audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC,
+                    AudioManager.ADJUST_RAISE,
+                    AudioManager.FLAG_REMOVE_SOUND_AND_VIBRATE | AudioManager.FLAG_SHOW_UI);
+            return true;
+        }
+        else if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN) {
+            AudioManager audioManager = (AudioManager) getBaseContext().getSystemService(
+                    Context.AUDIO_SERVICE);
+            audioManager.adjustStreamVolume(AudioManager.STREAM_MUSIC,
+                    AudioManager.ADJUST_LOWER,
+                    AudioManager.FLAG_REMOVE_SOUND_AND_VIBRATE | AudioManager.FLAG_SHOW_UI);
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+    public static String getYouTubeVideoId(String video_url) {
+
+        if (video_url != null && video_url.length() > 0) {
+
+            Uri video_uri = Uri.parse(video_url);
+            String video_id = video_uri.getQueryParameter("v");
+
+            if (video_id == null)
+                video_id = parseYoutubeVideoId(video_url);
+
+            return video_id;
+        }
+        return null;
+    }
+
+    public static String parseYoutubeVideoId(String youtubeUrl)
+    {
+        String video_id = null;
+        if (youtubeUrl != null && youtubeUrl.trim().length() > 0 &&
+                youtubeUrl.startsWith("http"))
+        {
+            // ^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/
+            String expression = "^.*((youtu.be" + "\\/)"
+                    + "|(v\\/)|(\\/u\\/w\\/)|(embed\\/)|(watch\\?))\\??v?=?([^#\\&\\?]*).*";
+            CharSequence input = youtubeUrl;
+            Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
+            Matcher matcher = pattern.matcher(input);
+            if (matcher.matches())
+            {
+                // Regular expression some how doesn't work with id with "v" at
+                // prefix
+                String groupIndex1 = matcher.group(7);
+                if (groupIndex1 != null && groupIndex1.length() == 11)
+                    video_id = groupIndex1;
+                else if (groupIndex1 != null && groupIndex1.length() == 10)
+                    video_id = "v" + groupIndex1;
+            }
+        }
+        return video_id;
+    }
+
+}
