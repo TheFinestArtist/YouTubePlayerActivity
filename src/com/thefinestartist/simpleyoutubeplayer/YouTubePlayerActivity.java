ba0ab551d9068258dfb861b2c8f06d70f62f8c1d
TheFinestArtist <liger745547@gmail.com>
2013-03-29 12:37:07 +0900

    Removed layout and added license

--- src/com/thefinestartist/simpleyoutubeplayer/YouTubePlayerActivity.java
+++ src/com/thefinestartist/simpleyoutubeplayer/YouTubePlayerActivity.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2013 The Finest Artist
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.thefinestartist.simpleyoutubeplayer;
 
@@ -26,22 +41,6 @@ import java.util.regex.Pattern;
 
 public class YouTubePlayerActivity extends YouTubeBaseActivity implements
         YouTubePlayer.OnInitializedListener, OnFullscreenListener {
-
-    /**
-     * Copyright 2013 The Finest Artist
-     *
-     * Licensed under the Apache License, Version 2.0 (the "License");
-     * you may not use this file except in compliance with the License.
-     * You may obtain a copy of the License at
-     *
-     *   http://www.apache.org/licenses/LICENSE-2.0
-     *
-     * Unless required by applicable law or agreed to in writing, software
-     * distributed under the License is distributed on an "AS IS" BASIS,
-     * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-     * See the License for the specific language governing permissions and
-     * limitations under the License.
-     */
 
     public static final String EXTRA_VIDEO_ID = "video_id";
     private static final boolean TOAST = false;
@@ -73,8 +72,9 @@ public class YouTubePlayerActivity extends YouTubeBaseActivity implements
 
         mAutoRotation = Settings.System.getInt(getContentResolver(),
                 Settings.System.ACCELEROMETER_ROTATION, 0) == 1;
-        
-        addContentView(mPlayerView, new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+
+        addContentView(mPlayerView, new LayoutParams(LayoutParams.MATCH_PARENT,
+                LayoutParams.MATCH_PARENT));
     }
 
     @Override
